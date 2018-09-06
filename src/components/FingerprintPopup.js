2bfc1df9578bdfa4bb37a2ee52b7f1fe03642008
liutao <423140368@qq.com>
2018-09-06 11:22:05 +0800

    hhh

--- src/components/FingerprintPopup.js
+++ src/components/FingerprintPopup.js
@@ -27,5 +27,5 @@ FingerprintPopup.propTypes = {
   handlePopupSuccessDismissed: PropTypes.func.isRequired,
   handlePopupFailDismissed: PropTypes.func.isRequired
 };
-//jhfjkdshfjksdhfjkds
+//jhfjkdshfjksdhfjkdsggh
 export default FingerprintPopup;
